084ee48e722521e993406a4dbec6ece5ea3a813e
MulieMes <mulemes81@gmail.com>
2022-03-28 12:57:23 +0300

    class comp changes

--- src/Components/ClassComponent.js
+++ src/Components/ClassComponent.js
@@ -3,7 +3,7 @@ import React from "react"
 
 class ClassComp extends React.Component{
     render(){
-      return <p>This is a Class Component</p>
+      return <p>This is a Class Component Rendered!</p>
     }
 }
 export default ClassComp; 
